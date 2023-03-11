fd435fa3fdb5de0b8ab03ee6ff7dc687ff8163a8
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2023-03-11 18:09:39 +0200

    removed styles from final

---
+++ src/components/SimpleCounter.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+
+function increase() {
+  count.value++
+}
+
+function decrease() {
+  count.value--
+}
+</script>
+
+<template>
+  <h1>Count: {{ count }}</h1>
+  <div>
+    <button @click="increase">Increase</button>
+    <button @click="decrease">Decrease</button>
+  </div>
+</template>
